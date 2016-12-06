4a96fd21fde246e5fe30e341ca4f3e5ba14c81ad
TitiMoby <titimoby@gmail.com>
2016-12-06 23:05:23 +0100

    add json config file

--- software/setup.py
+++ software/setup.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python
 
+import sys
+import re
+
 from setuptools import setup, find_packages
 
+def version():
+    with open('darwinoppy/_version.py') as f:
+        return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
+
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
 
 setup(name='darwinoppy',
-      version='TO BE COMPLETED',
+      version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 2.11', 'poppy-creature >= 1.8'],
+      install_requires=['pypot >= 2.10', 'poppy-creature'],
 
-      author='TO BE COMPLETED',
-      author_email='TO BE COMPLETED',
-      description='TO BE COMPLETED',
-      url='TO BE COMPLETED',
-      license='TO BE COMPLETED',
+      setup_requires=['setuptools_git >= 0.3', ],
+
+      include_package_data=True,
+      exclude_package_data={'': ['README', '.gitignore']},
+
+      zip_safe=False,
+
+      author='Thierry Chantier',
+      author_email='titimoby@gmail.com',
+      description='Darwinoppy software library for Poppy project',
+      url='https://github.com/titimoby/darwinoppy',
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
       )
--- software/tools/monitor.py
+++ software/tools/monitor.py
@@ -9,22 +9,22 @@ def port_list():
 
 @route('/')
 def motor_list():
-    motors = {1 : {'id':1, 'name':"a", 'detected':False, 'position':None},
-    2 : {'id':2, 'name':"a", 'detected':False, 'position':None},
-    3 : {'id':3, 'name':"a", 'detected':False, 'position':None},
-    4 : {'id':4, 'name':"a", 'detected':False, 'position':None},
-    5 : {'id':5, 'name':"a", 'detected':False, 'position':None},
-    6 : {'id':6, 'name':"a", 'detected':False, 'position':None},
-    7 : {'id':7, 'name':"a", 'detected':False, 'position':None},
-    8 : {'id':8, 'name':"a", 'detected':False, 'position':None},
-    9 : {'id':9, 'name':"a", 'detected':False, 'position':None},
-    10 : {'id':10, 'name':"a", 'detected':False, 'position':None},
-    11 : {'id':11, 'name':"a", 'detected':False, 'position':None},
-    12 : {'id':12, 'name':"a", 'detected':False, 'position':None},
-    13 : {'id':13, 'name':"a", 'detected':False, 'position':None},
-    14 : {'id':14, 'name':"a", 'detected':False, 'position':None},
-    15 : {'id':15, 'name':"a", 'detected':False, 'position':None},
-    16 : {'id':16, 'name':"a", 'detected':False, 'position':None}}
+    motors = {1 : {'id':1, 'name':"r_shoulder_y", 'detected':False, 'position':None},
+    2 : {'id':2, 'name':"l_shoulder_y", 'detected':False, 'position':None},
+    3 : {'id':3, 'name':"r_shoulder_x", 'detected':False, 'position':None},
+    4 : {'id':4, 'name':"l_shoulder_x", 'detected':False, 'position':None},
+    5 : {'id':5, 'name':"r_elbow_x", 'detected':False, 'position':None},
+    6 : {'id':6, 'name':"l_elbow_x", 'detected':False, 'position':None},
+    7 : {'id':7, 'name':"r_hip_x", 'detected':False, 'position':None},
+    8 : {'id':8, 'name':"l_hip_x", 'detected':False, 'position':None},
+    9 : {'id':9, 'name':"r_hip_y", 'detected':False, 'position':None},
+    10 : {'id':10, 'name':"l_hip_y", 'detected':False, 'position':None},
+    11 : {'id':11, 'name':"r_knee_y", 'detected':False, 'position':None},
+    12 : {'id':12, 'name':"l_knee_y", 'detected':False, 'position':None},
+    13 : {'id':13, 'name':"r_ankle_y", 'detected':False, 'position':None},
+    14 : {'id':14, 'name':"l_ankle_y", 'detected':False, 'position':None},
+    15 : {'id':15, 'name':"r_ankle_x", 'detected':False, 'position':None},
+    16 : {'id':16, 'name':"l_ankle_x", 'detected':False, 'position':None}}
     dxl_io = pypot.dynamixel.Dxl320IO('/dev/ttyAMA0')
     detected = (dxl_io.scan(range(60)))
 
@@ -38,22 +38,22 @@ def motor_list():
 @route('/simulates')
 def simulates():
     print("simulates")
-    motors = {1 : {'id':1, 'name':"a", 'detected':False, 'position':12},
-    2 : {'id':2, 'name':"a", 'detected':False, 'position':25},
-    3 : {'id':3, 'name':"a", 'detected':False, 'position':38},
-    4 : {'id':4, 'name':"a", 'detected':False, 'position':52},
-    5 : {'id':5, 'name':"a", 'detected':False, 'position':44},
-    6 : {'id':6, 'name':"a", 'detected':False, 'position':10},
-    7 : {'id':7, 'name':"a", 'detected':False, 'position':25},
-    8 : {'id':8, 'name':"a", 'detected':False, 'position':4},
-    9 : {'id':9, 'name':"a", 'detected':False, 'position':-50},
-    10 : {'id':10, 'name':"a", 'detected':False, 'position':-12},
-    11 : {'id':11, 'name':"a", 'detected':False, 'position':0},
-    12 : {'id':12, 'name':"a", 'detected':False, 'position':32},
-    13 : {'id':13, 'name':"a", 'detected':False, 'position':-8},
-    14 : {'id':14, 'name':"a", 'detected':False, 'position':None},
-    15 : {'id':15, 'name':"a", 'detected':False, 'position':2},
-    16 : {'id':16, 'name':"a", 'detected':False, 'position':-3}}
+    motors = {1 : {'id':1, 'name':"r_shoulder_y", 'detected':False, 'position':2},
+    2 : {'id':2, 'name':"l_shoulder_y", 'detected':False, 'position':45},
+    3 : {'id':3, 'name':"r_shoulder_x", 'detected':False, 'position':-4},
+    4 : {'id':4, 'name':"l_shoulder_x", 'detected':False, 'position':None},
+    5 : {'id':5, 'name':"r_elbow_x", 'detected':False, 'position':-54},
+    6 : {'id':6, 'name':"l_elbow_x", 'detected':False, 'position':21},
+    7 : {'id':7, 'name':"r_hip_x", 'detected':False, 'position':124},
+    8 : {'id':8, 'name':"l_hip_x", 'detected':False, 'position':-100},
+    9 : {'id':9, 'name':"r_hip_y", 'detected':False, 'position':24},
+    10 : {'id':10, 'name':"l_hip_y", 'detected':False, 'position':78},
+    11 : {'id':11, 'name':"r_knee_y", 'detected':False, 'position':-54},
+    12 : {'id':12, 'name':"l_knee_y", 'detected':False, 'position':None},
+    13 : {'id':13, 'name':"r_ankle_y", 'detected':False, 'position':-54},
+    14 : {'id':14, 'name':"l_ankle_y", 'detected':False, 'position':24},
+    15 : {'id':15, 'name':"r_ankle_x", 'detected':False, 'position':8},
+    16 : {'id':16, 'name':"l_ankle_x", 'detected':False, 'position':78}}
 
     return template('monitor.tpl', motors=motors)
 
